17d0102473e0f6678a952e5d8e9dcdeaaf36b3db
Matheus Enrique Alves <menrique@ic.ufal.br>
2020-06-24 22:20:44 -0300

    commit site

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen(8080, function(){
+var port = process.env.PORT || 8080;
+
+connect().use(serveStatic(__dirname)).listen(port, function(){
 console.log('Servidor rodando!');
 });
